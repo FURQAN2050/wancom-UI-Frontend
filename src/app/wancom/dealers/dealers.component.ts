8b9ed409142a8b44f5fd06c1d8d2e3a16780a59e
MUHAMMAD FURQAN <31932590+FURQAN2050@users.noreply.github.com>
2019-10-30 20:39:43 +0500

    Dealers page

--- src/app/wancom/dealers/dealers.component.ts
+++ src/app/wancom/dealers/dealers.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dealers.component.scss']
 })
 export class DealersComponent implements OnInit {
-
+  dealersList=[
+    {no:1,name:"AR Net",subDealers:"20"},
+    {no:2,name:"Safoora Net",subDealers:"2"},
+    {no:3,name:"UZ Net",subDealers:"30"},
+    {no:4,name:"Virgo Net",subDealers:"9"},
+    {no:5,name:"Waseem Net",subDealers:"12"},
+    {no:6,name:"Rizwi Link",subDealers:"14"},
+    {no:7,name:"ZN Network",subDealers:"17"},
+  ]
   constructor() { }
 
   ngOnInit() {
