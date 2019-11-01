5a575d3a4a12852d954348a4f05a7d4deca1034e
MUHAMMAD FURQAN <31932590+FURQAN2050@users.noreply.github.com>
2019-11-01 17:17:05 +0500

    addinng mat differnt modules in app.modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DealersMainComponent } from './wancom/Dealers/dealers-main/dealers-main.component';
+import { AddDealerComponent } from './wancom/Dealers/add-dealer/add-dealer.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'dealers',component:DealersMainComponent},
+  {path:'addDealer',component:AddDealerComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// custom components
+import { DealersMainComponent } from './wancom/Dealers/dealers-main/dealers-main.component';
+import { AddDealerComponent } from './wancom/Dealers/add-dealer/add-dealer.component';
+
+//modals
+import { AddPackageModalComponent } from './wancom/Modals/add-package-modal/add-package-modal.component';
+
+// angular material exports
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDialogModule} from '@angular/material/dialog';
+
 @NgModule({
+  entryComponents:[AddPackageModalComponent],
   declarations: [
-    AppComponent
+    AppComponent,
+    DealersMainComponent,
+    AddDealerComponent,
+    AddPackageModalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
