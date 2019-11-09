cfe9d5260da8796aa02a45b32de00d19423435b7
MUHAMMAD FURQAN <31932590+FURQAN2050@users.noreply.github.com>
2019-11-09 22:50:55 +0500

    update landing page,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
 
+import { Component } from '@angular/core';
+import { LoopBackAuth } from './shared/sdk';
+import { LoopBackConfig } from './shared/sdk/index';
+import { BASE_URL, API_VERSION } from './base.url';
+import { GetAllPackagesService } from './wancom/Services/packages/getAllPackages/get-all-packages.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(public LoopBackAuthService:LoopBackAuth,public PackagesListService:GetAllPackagesService){
+    LoopBackConfig.setBaseURL(BASE_URL);
+    LoopBackConfig.setApiVersion(API_VERSION);
+    this.initiatePackagesService()
+  }
   title = 'Wancom';
+
+  initiatePackagesService(){
+    this.PackagesListService.setAllPackages();
+  }
 }
