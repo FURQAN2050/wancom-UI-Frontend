b09336d869d5dfee2e18c67948f2a0d30ba8e419
ArsalanWahidAsghar <arsalanwahid1993@gmail.com>
2019-11-11 23:18:14 +0500

    nav bar and side bar restructure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { DealersMainComponent } from './wancom/Dealers/dealers-main/dealers-main
 import { AddDealerComponent } from './wancom/Dealers/add-dealer/add-dealer.component';
 import { PackagesMainComponent } from './wancom/Packages/packages-main/packages-main.component';
 import { SubDealersMainComponent } from './wancom/SubDealers/sub-dealers-main/sub-dealers-main.component';
-
+import { DealersListComponent } from './wancom/Dealers/dealers-list/dealers-list.component';
 
 
 const routes: Routes = [
   {path:'dealers',component:DealersMainComponent},
   {path:'addDealer',component:AddDealerComponent},
   {path:'packages',component:PackagesMainComponent},
-  {path:'subdealers',component:SubDealersMainComponent}
+  {path:'subdealers',component:SubDealersMainComponent},
+  {path:'',redirectTo:'dealers',pathMatch:'full'}
 
 ];
 
